5fcc8831a0ab4e1915aecc64e8c66771a4daa7ce
shaher <shaher@192.168.1.104>
2018-08-04 22:50:19 -0700

    Update

--- clientreview/src/main/java/com/sogeti/clientreview/ClientreviewApplication.java
+++ clientreview/src/main/java/com/sogeti/clientreview/ClientreviewApplication.java
@@ -1,8 +1,14 @@
 package com.sogeti.clientreview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ClientreviewApplication {
 
